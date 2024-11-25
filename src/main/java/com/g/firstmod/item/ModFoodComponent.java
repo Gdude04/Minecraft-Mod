a7fe477c55524907bfba71464ac392b237cd783e
Gdude04 <garvinfrancois@gmail.com>
2024-11-25 08:08:29 -0500

    Part 9

--- src/main/java/com/g/firstmod/item/ModFoodComponent.java
+++ src/main/java/com/g/firstmod/item/ModFoodComponent.java
@@ -6,5 +6,5 @@ import net.minecraft.item.FoodComponent;
 
 public class ModFoodComponent {
     public static final FoodComponent TOMATO = new FoodComponent.Builder().hunger(3).saturationModifier(0.25f)
-            .statusEffects(new StatusEffectInstance(StatusEffects.SPEED, 3000), 100).build();
+            .statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 3000), 100).build();
 }
