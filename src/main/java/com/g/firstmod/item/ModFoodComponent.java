4abc65a2747df128f60489dc2852d2ee9e5effa8
Gdude04 <garvinfrancois@gmail.com>
2024-11-25 18:30:53 -0500

    Part 11

--- src/main/java/com/g/firstmod/item/ModFoodComponent.java
+++ src/main/java/com/g/firstmod/item/ModFoodComponent.java
@@ -5,6 +5,6 @@ import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.FoodComponent;
 
 public class ModFoodComponent {
-    public static final FoodComponent TOMATO = new FoodComponent.Builder().hunger(3).saturationModifier(0.25f)
+    public static final FoodComponent TOMATO = new FoodComponent.Builder().hunger(8).saturationModifier(0.25f)
             .statusEffect(new StatusEffectInstance(StatusEffects.SPEED, 3000), 100).build();
 }
--- src/main/java/com/g/firstmod/item/custom/MetalDetectorItem.java
+++ src/main/java/com/g/firstmod/item/custom/MetalDetectorItem.java
@@ -1,14 +1,21 @@
 package com.g.firstmod.item.custom;
 
+import com.g.firstmod.util.ModTags;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class MetalDetectorItem extends Item {
     public MetalDetectorItem(Settings settings) {
@@ -50,6 +57,12 @@ public class MetalDetectorItem extends Item {
     }
 
     private boolean isValueableBlock(BlockState state) {
-        return state.isOf(Blocks.IRON_ORE) || state.isOf(Blocks.DIAMOND_ORE);
+        return state.isIn(ModTags.Blocks.METAL_DETECTOR_DETECTABLE_BLOCKS);
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, @Nullable World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(Text.translatable("tooltip.firstmod.metal_detector.tooltip"));
+        super.appendTooltip(stack, world, tooltip, context);
     }
 }
