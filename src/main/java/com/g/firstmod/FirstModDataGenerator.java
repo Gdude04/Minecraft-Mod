98bee1bbab222f9bfdf2f9c5971eefcc3ad2cffc
Gdude04 <garvinfrancois@gmail.com>
2024-11-26 14:16:41 -0500

    Part 12

--- src/main/java/com/g/firstmod/FirstModDataGenerator.java
+++ src/main/java/com/g/firstmod/FirstModDataGenerator.java
@@ -1,11 +1,19 @@
 package com.g.firstmod;
 
+import com.g.firstmod.datagen.*;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class FirstModDataGenerator implements DataGeneratorEntrypoint {
 	@Override
 	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+		FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
+
+		pack.addProvider(ModBlockTagProvider::new);
+		pack.addProvider(ModItemTagProvider::new);
+		pack.addProvider(ModLootTableProvider::new);
+		pack.addProvider(ModModelProvider::new);
+		pack.addProvider(ModRecipeProvider::new);
 
 	}
 }
---
+++ src/main/java/com/g/firstmod/datagen/ModItemTagProvider.java
@@ -0,0 +1,19 @@
+package com.g.firstmod.datagen;
+
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModItemTagProvider extends FabricTagProvider.ItemTagProvider {
+    public ModItemTagProvider(FabricDataOutput output, CompletableFuture registriesFuture) {
+        super(output, registriesFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup wrapperLookup) {
+
+    }
+}
