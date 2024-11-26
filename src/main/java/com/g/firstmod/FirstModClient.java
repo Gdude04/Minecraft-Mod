d696959fb89e303ac8c7e4a05c15a67ade9a0ab0
Gdude04 <garvinfrancois@gmail.com>
2024-11-26 15:47:08 -0500

    Part 13

--- src/main/java/com/g/firstmod/FirstModClient.java
+++ src/main/java/com/g/firstmod/FirstModClient.java
@@ -1,10 +1,15 @@
 package com.g.firstmod;
 
+import com.g.firstmod.block.ModBlocks;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.render.RenderLayer;
 
 public class FirstModClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.RUBY_DOOR, RenderLayer.getCutout());
+        BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.RUBY_TRAPDOOR, RenderLayer.getCutout());
 
     }
 }
