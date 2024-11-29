d38661b56fabc5fc8e8f99928c8320acc3b728db
Gdude04 <garvinfrancois@gmail.com>
2024-11-29 12:21:16 -0500

    Armor Effects

--- src/main/java/com/g/firstmod/item/ModItems.java
+++ src/main/java/com/g/firstmod/item/ModItems.java
@@ -2,6 +2,7 @@ package com.g.firstmod.item;
 
 import com.g.firstmod.FirstMod;
 import com.g.firstmod.item.custom.MetalDetectorItem;
+import com.g.firstmod.item.custom.ModArmorItem;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.itemgroup.v1.FabricItemGroupEntries;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
@@ -34,7 +35,7 @@ public class ModItems {
             new HoeItem(ModToolMaterial.RUBY,0,0f, new  FabricItemSettings()));
 
     public static final Item RUBY_HELMET = registerItem("ruby_helmet",
-            new ArmorItem(ModArmorMaterials.RUBY, ArmorItem.Type.HELMET, new  FabricItemSettings()));
+            new ModArmorItem(ModArmorMaterials.RUBY, ArmorItem.Type.HELMET, new  FabricItemSettings()));
     public static final Item RUBY_CHESTPLATE = registerItem("ruby_chestplate",
             new ArmorItem(ModArmorMaterials.RUBY, ArmorItem.Type.CHESTPLATE, new  FabricItemSettings()));
     public static final Item RUBY_LEGGINGS = registerItem("ruby_leggings",
