c88c8ce689d88a2aef3b9fcf32599f18c1e2a654
Gdude04 <garvinfrancois@gmail.com>
2024-11-30 14:23:09 -0500

    Suspicious Sand Drops

--- src/main/java/com/g/firstmod/util/ModLootTableModifiers.java
+++ src/main/java/com/g/firstmod/util/ModLootTableModifiers.java
@@ -3,18 +3,29 @@ package com.g.firstmod.util;
 import com.g.firstmod.item.ModItems;
 import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
 import net.minecraft.loot.LootPool;
+import net.minecraft.loot.LootTable;
 import net.minecraft.loot.condition.RandomChanceLootCondition;
 import net.minecraft.loot.entry.ItemEntry;
+import net.minecraft.loot.entry.LootPoolEntry;
 import net.minecraft.loot.function.SetCountLootFunction;
 import net.minecraft.loot.provider.number.ConstantLootNumberProvider;
 import net.minecraft.loot.provider.number.UniformLootNumberProvider;
 import net.minecraft.util.Identifier;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class ModLootTableModifiers {
     private static final Identifier JUNGLE_TEMPLE_ID =
             new Identifier("minecraft", "chests/jungle_temple");
     private static final Identifier CREEPER_ID =
             new Identifier("minecraft", "entities/creeper");
+    private static final Identifier SUSPICIOUS_SAND_ID =
+            new Identifier("minecraft","archaeology/desert_pyramid");
+    private static final Logger log = LoggerFactory.getLogger(ModLootTableModifiers.class);
 
 
     public static void modifyLootTables(){
@@ -39,5 +50,19 @@ public class ModLootTableModifiers {
                 tableBuilder.pool(poolBuilder.build());
             }
         }));
+
+        LootTableEvents.REPLACE.register((resourceManager, lootManager, id, original, source) ->{
+            if (SUSPICIOUS_SAND_ID.equals(id)){
+                List<LootPoolEntry> entries = new ArrayList<>(Arrays.asList(original.pools[0].entries));
+                entries.add(ItemEntry.builder(ModItems.METAL_DETECTOR).build());
+                entries.add(ItemEntry.builder(ModItems.RUBY).build());
+
+                LootPool.Builder pool = LootPool.builder().with(entries);
+                return LootTable.builder().pool(pool).build();
+            }
+
+            return null;
+
+        });
     }
 }
